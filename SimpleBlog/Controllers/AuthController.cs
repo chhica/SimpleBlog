7e8060a4640c585a2dbef02526c9dec8c0d9e6bc
Gee <chhica@live.com>
2017-04-05 00:47:56 -0700

    Added basic login form.

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,7 +14,33 @@ namespace SimpleBlog.Controllers
         {
             //return Content("Login!");
             //instead of returning content Login, it will return a view
-            return View();
+            return View(new AuthLogin
+            {
+                Test = "This is my test value set in my controller."
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            //use when non-strongly typed
+            //return Content("Hey there, " + form.Username);
+
+            ///when strongly typed:
+            form.Test = "This is a value set in my post action.";
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            //add additional checks to our action results
+            //if checks are related to business logic
+            if (form.Username != "gee")
+            {
+                ModelState.AddModelError("Username", "Username or password isn't 20% cooler.");
+                return View(form);
+            }
+
+            return Content("The form is valid.");
         }
     }
 }
