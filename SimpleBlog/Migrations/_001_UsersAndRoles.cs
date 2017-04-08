b7fddb20b58495c9807f9437e812ce4039e2ac1e
Gee <chhica@live.com>
2017-04-08 13:02:17 -0700

    Versioned DB with Fluent Migrator.

---
+++ SimpleBlog/Migrations/_001_UsersAndRoles.cs
@@ -0,0 +1,48 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Migrations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+        /// <summary>
+        /// when rollbacking changes to db
+        /// </summary>
+        public override void Down()
+        {
+            //first delete all the related tables
+            Delete.Table("role_users");
+            Delete.Table("roles");
+            Delete.Table("users");
+        }
+
+        /// <summary>
+        /// do modifications to the db
+        /// </summary>
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("username").AsString(128)
+                .WithColumn("email").AsCustom("VARCHAR(256)")//AsString(256)
+                .WithColumn("password_hash").AsString(128);
+
+            Create.Table("roles")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("name").AsString(128);
+
+            //pivot table to associate users and roles
+            //with referencial integrity
+            //when a user is deleted, it cascades on the role_users
+            Create.Table("role_users")
+                .WithColumn("user_id").AsInt32().ForeignKey("users", "id").OnDelete(Rule.Cascade)
+                .WithColumn("role_id").AsInt32().ForeignKey("roles", "id").OnDelete(Rule.Cascade);
+               
+        }
+    }
+}
---
+++ SimpleBlog/Tools/DeployDb-Dev.bat
@@ -0,0 +1,2 @@
+﻿REM dont forget to update the path
+..\..\packages\FluentMigrator.1.6.2\tools\migrate.exe --db=mysql --target=..\bin\simpleblog.dll --configPath=..\Web.config -c=MainDb
